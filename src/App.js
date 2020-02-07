95477a6c5ebd32d0901bdc870c15914a355519ce
zimmy125 <zimmy125@azimmerman05.thezimmermanhome.com>
2020-02-06 21:10:52 -0600

    More Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRoute,Route,Switch } from 'react-router-dom';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
 
 function App() {
   return (
